600a9b913b3cf51e1356ba85f401264171b4047e
uag515 <uag515@sina.com>
2019-07-05 18:01:07 +0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by UAG on 2019/7/5.
+//  Copyright © 2019 DC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
