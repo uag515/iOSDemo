2fe448197d1027417d941de2f87b762f75d8d0af
uag515 <uag515@sina.com>
2019-07-07 16:01:53 +0800

    edit: add basic ui

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       // handle the field's user input throth txet delegete callbacks
+        //  When a ViewController instance is loaded, it sets itself as the delegate of its nameTextField property.
+        nameTextField.delegate = self;
     }
-
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hide the keyboard
+        textField.resignFirstResponder();
+        return true;
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // do some thing
+        mealNameLabel.text = textField.text;
+    }
+    //MARK: action
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "chicken chicken !"
+    }
+    
 }
 
